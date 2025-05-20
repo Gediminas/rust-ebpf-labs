66dc9df3de8c46f96c64bf53ce7066b4fec4fdfa
Gediminas Lūžys <163294+gediminas@users.noreply.github.com>
2025-05-20 18:20:19 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Rust and eBPF Playground
---
+++ shell.nix
@@ -0,0 +1,33 @@
+{ nixpkgs ? import <nixpkgs> {} }:
+let
+  unstable = import (fetchTarball "https://nixos.org/channels/nixos-unstable/nixexprs.tar.xz") { };
+  # rustupFlagFile = ".lock-rustup";
+in
+with nixpkgs; mkShell {
+  buildInputs = [
+    # unstable.rustup
+    # unstable.cargo-generate
+    # llvmPackages_19.clang
+    just
+
+    # other tools
+    bpftools
+    xdp-tools
+    stress-ng
+  ];
+
+  shellHook = ''
+    unset TMPDIR || set -e TMPDIR || true
+
+    echo ""
+    echo    ">>>>> rust_ebpf_playground:"
+    echo    ">>>>>"
+    echo -n ">>>>> "; gcc   --version | head -n1 | awk '{print $1"  ", $3, $2}'
+    echo -n ">>>>> "; rustup --version 2>/dev/null | head -n1
+    echo -n ">>>>> "; cargo --version
+    echo -n ">>>>> "; cargo +nightly --version
+    echo -n ">>>>> "; rust-analyzer --version
+    echo -n ">>>>> "; bpf-linker --version
+    echo ""
+  '';
+}
